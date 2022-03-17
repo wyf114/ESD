b0b358f4ee88d3fb586895407f5efca574b05537
Megan Thong <meganthong@outlook.com>
2022-03-17 15:08:13 +0800

    add spring boot microservice - functioning but not useful rn

---
+++ selection/src/test/java/flight/search/selection/SelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package flight.search.selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
