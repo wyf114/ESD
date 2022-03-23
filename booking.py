2d9b14e20a2b0002828655db0cb1265eea363f71
ellayang0618 <66121210+ellayang0618@users.noreply.github.com>
2022-03-23 21:09:37 +0800

    booking service, need to add db and route in

---
+++ booking.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/booking'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+CORS(app)  
+
+
+################## to be done #############
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
