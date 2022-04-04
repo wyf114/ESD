b591963b4d526db6562e4f7b5ee0537c9f076378
Megan Thong <meganthong@outlook.com>
2022-04-04 14:21:08 +0800

    update docker image for java

--- selection/src/main/java/flight/search/selection/SelectionApplication.java
+++ selection/src/main/java/flight/search/selection/SelectionApplication.java
@@ -20,7 +20,7 @@ public class SelectionApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5500");
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
